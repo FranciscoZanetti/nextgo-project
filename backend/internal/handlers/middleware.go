762e95d41f662ccfaea82559eed85d5c5e17af31
Francisco Zanetti <63816898+FranciscoZanetti@users.noreply.github.com>
2025-05-19 18:45:50 -0300

    quick fix

--- backend/internal/handlers/middleware.go
+++ backend/internal/handlers/middleware.go
@@ -12,8 +12,6 @@ func EnableCORS(next http.Handler) http.Handler {
   	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		frontendOrigin := os.Getenv("FRONTEND_URL")
 		w.Header().Set("Access-Control-Allow-Origin", frontendOrigin)
-
-    	w.Header().Set("Access-Control-Allow-Origin", "*")
     	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
     	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
