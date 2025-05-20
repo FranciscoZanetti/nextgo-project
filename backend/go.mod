d1a12ab3dcd2d48a3e22dd0e6b6dcb3f5cb6a6a5
Francisco Zanetti <63816898+FranciscoZanetti@users.noreply.github.com>
2025-05-20 01:35:57 -0300

    sentry backend

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,12 @@ module github.com/FranciscoZanetti/nextgo-project/backend
 
 go 1.20
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/getsentry/sentry-go v0.30.0
+	github.com/lib/pq v1.10.9
+)
+
+require (
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -5,15 +5,30 @@ import (
     "net/http"
 	"os"
 
+    "github.com/getsentry/sentry-go"
+	sentryhttp "github.com/getsentry/sentry-go/http"
     "github.com/FranciscoZanetti/nextgo-project/backend/internal/database"
 	"github.com/FranciscoZanetti/nextgo-project/backend/internal/handlers"
 )
 
 func main() {
+    err := sentry.Init(sentry.ClientOptions{
+		Dsn:              os.Getenv("SENTRY_DSN"),
+		TracesSampleRate: 1.0,
+	})
+	if err != nil {
+		log.Fatalf("Sentry initialization failed: %v\n", err)
+	}
+	defer sentry.Flush(2 * time.Second)
+
 	database.InitDB()
 
 	port := os.Getenv("PORT")
 	url := os.Getenv("URL")
+
+    sentryHandler := sentryhttp.New(sentryhttp.Options{
+		Repanic: true,
+	})
 
     mux := http.NewServeMux()
 
